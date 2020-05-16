45bfcd99b81fc98d72c63e90cebb0daf2aaa3d4f
Enrique <edavalos@ucsc.edu>
2020-05-15 21:10:10 -0700

    added comments

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
+// loads all issues
 function fetchIssues() {
+    // gets issues list from local storage
     let issues = JSON.parse(localStorage.getItem('issues'));
     let issuesList = document.getElementById('issuesList');
 
+    // this is where new issues' HTML will be put
     issuesList.innerHTML = '';
 
+    // loop thru all issues
     for (let i = 0; i < issues.length; i++) {
+        // grab information for each issue
         let id = issues[i].id;
         let desc = issues[i].description;
         let severity = issues[i].severity;
         let assignedTo = issues[i].assignedTo;
         let status = issues[i].status;
 
+        // if issue has a team assigned, throw in the team icon
         let assignedIcon = '';
         if (assignedTo != '') assignedIcon = '<span class="glyphicon glyphicon-user"></span> ';
 
+        // create new HTML for issue box w/ its info
         issuesList.innerHTML += '<div class="well">'+
                                     '<h6>Issue ID: ' + id + '</h6>'+
                                     '<p><span class="label label-info">' + status + '</span></p>'+
@@ -27,21 +34,26 @@ function fetchIssues() {
     }
 }
 
+// registers button listener
 document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
 
+// gets info from form and saves it to new issue
 function saveIssue(event) {
 
+    // generate info for new issue from form fields
     let issueID = generateID(4);
     let issueDesc = document.getElementById('issueDescInput').value;
     let issueSeverity = document.getElementById('issueSeverityInput').value;
     let issueAssignedTo = document.getElementById('issueAssignedToInput').value;
     let issueStatus = 'Open';
 
+    // yell at you if you leave description blank
     if (issueDesc == '') {
         alert("Please enter a description");
         return;
     }
 
+    // create issue object
     let issue = {
         id: issueID,
         description: issueDesc,
@@ -50,29 +62,36 @@ function saveIssue(event) {
         status: issueStatus
     }
 
+    // if issue object doesn't exist, create it in local storage
     if (localStorage.getItem('issues') === null) {
         let issues = [];
         issues.push(issue);
         localStorage.setItem('issues', JSON.stringify(issues));
     }
 
+    // else, just edit it
     else {
         let issues = JSON.parse(localStorage.getItem('issues'));
         issues.push(issue);
         localStorage.setItem('issues', JSON.stringify(issues));
     }
 
+    // reset form
     document.getElementById('issueInputForm').reset();
 
+    // reload issues (show them)
     fetchIssues();
 
 }
 
+// for when close-issue button is pressed
 function setStatusClosed(id) {
     let issues = JSON.parse(localStorage.getItem('issues'));
     
+    // find issue
     for(let i = 0; i < issues.length; i++) {
       if (issues[i].id == id) {
+        // just change the tag's text
         issues[i].status = "Closed";
       }
     }
@@ -82,11 +101,14 @@ function setStatusClosed(id) {
     fetchIssues();
 }
 
+// for when delete-issue button is pressed
 function deleteIssue(id) {
     let issues = JSON.parse(localStorage.getItem('issues'));
     
+    // find issue
     for(let i = 0; i < issues.length; i++) {
       if (issues[i].id == id) {
+        // delete it
         issues.splice(i, 1);
       }
     }
@@ -96,6 +118,7 @@ function deleteIssue(id) {
     fetchIssues();
 }
 
+// generate new random id
 function generateID(length) {
     let id = '';
     for (let i = 0; i < length; i++) {
--- search.js
+++ search.js
@@ -1,46 +1,63 @@
+// loads all issues that match search
 function displayResults(mode, key) {
+    // gets issues list from local storage
     let issues = JSON.parse(localStorage.getItem('issues'));
     let issuesList = document.getElementById('results');
 
+    // this is where new issues' HTML will be put
     issuesList.innerHTML = '';
+    // this is where search terms are put
     keys = key.split(" ");
 
+    // loop thru all issues
     for (let i = 0; i < issues.length; i++) {
+        // grab information for each issue
         let id = issues[i].id;
         let desc = issues[i].description;
         let severity = issues[i].severity;
         let assignedTo = issues[i].assignedTo;
         let status = issues[i].status;
 
+        // if search is by ID
         if (mode === 'id') {
             found = false;
             for (const subkey in keys) {
+                // make note that an issue's id contained a searched id
                 if (id.includes(keys[subkey])) found = true;
             }
+            // if no issue was found, skip over creating html for it
             if (!found) continue;
         }
 
+        // if search is by description
         if (mode === 'desc') {
             found = false;
             for (const subkey in keys) {
+                // make note that an issue's description contained a searched term
                 if (desc.toLowerCase().includes(keys[subkey].toLowerCase())) found = true;
             }
+            // if no issue was found, skip over creating html for it
             if (!found) continue;
         }
 
 
+        // make searched ids bold
         for (const subkey in keys) {
             id = id.replace(keys[subkey], "<b>" + keys[subkey] + "</b>");
         }
 
+        // make searched descriptions bold
         for (const subkey in keys) {
+            // ...unless the search term happens to be the bold tag
             if ((keys[subkey] === '<') || (keys[subkey] === '/') || (keys[subkey] === 'b') || (keys[subkey] === '>')) continue;
             desc = desc.replace(keys[subkey], "<b>" + keys[subkey] + "</b>");
         }
 
+        // if issue has a team assigned, throw in the team icon
         let assignedIcon = '';
         if (assignedTo != '') assignedIcon = '<span class="glyphicon glyphicon-user"></span> ';
 
+        // create new HTML for issue box w/ its info
         issuesList.innerHTML += '<div class="well">'+
                                     '<h6>Issue ID: ' + id + '</h6>'+
                                     '<p><span class="label label-info">' + status + '</span></p>'+
@@ -54,24 +71,32 @@ function displayResults(mode, key) {
     }
 }
 
+// searches for issue given some IDs
 function lookForID() {
 
+    // get id list from form input
     let key = document.getElementById('IDSearchInput').value;
 
+    // if input is not a number, yell at you
     if (!(/^\d+$/.test(key.replace(" ", "")))) {
         alert("IDs only contain numbers.");
         return;
     }
 
+    // call issue HTML loader
     displayResults('id', key);
 
+    // reset form
     document.getElementById('searchByID').reset();
 }
 
+// searches for issue given some descriptions
 function lookForDesc() {
 
+    // get desc list from form input & call HTML loader
     let key = document.getElementById('DescSearchInput').value;
     displayResults('desc', key);
 
+    // reset form
     document.getElementById('searchByDesc').reset();
 }
